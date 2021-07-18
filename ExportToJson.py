4a39ec9791f528d7496860ce7b2e3351bcd5ec65
Nathan Giusti <bigbadbyte2@gmail.com>
2021-07-17 20:49:08 -0400

    CL Tool to extract report layout from PBIX to file

---
+++ ExportToJson.py
@@ -0,0 +1,27 @@
+import json
+import sys
+import zipfile
+
+# Usage
+# python ExportToJson.py [pbix file location] [output file location]
+# Ex: python ExportToJson.py Folder/MyPbixFile.pbix MyReportJson.json
+
+
+def main():
+
+    file_location = sys.argv[1]
+    output_path = sys.argv[2]
+
+    zf = zipfile.ZipFile(file_location)
+    data = json.loads(zf.read('Report/Layout').decode('utf-16-le'))
+    data['config'] = json.loads(data['config'])
+    for section in data['sections']:
+        for visual_container in section['visualContainers']:
+            for key in ['config', 'filters', 'query', 'dataTransforms']:
+                visual_container[key] = json.loads(visual_container[key])
+    with open(output_path, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
